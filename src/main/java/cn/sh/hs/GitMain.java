a64b9ac8fc0a2709b971122d35ccc2195649f41e
zhudaohai <31030192+zhudaohai@users.noreply.github.com>
2017-08-18 17:43:28 +0800

    Update GitMain.java

--- src/main/java/cn/sh/hs/GitMain.java
+++ src/main/java/cn/sh/hs/GitMain.java
@@ -10,7 +10,7 @@ public class GitMain {
 
     public static void main(String[] args) {
         System.out.print("新创的一个开发项目！1111111");
-
+        System.out.print("88888888888888");
         System.out.print("22222223333333333333333444444444");
         System.out.print("222222233333333333333355555");
         System.out.print("666");
