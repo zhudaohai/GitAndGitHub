e3814de1ae7686b1a9ccbc887a1f52dc2253f2f3
zhudaohai <31030192+zhudaohai@users.noreply.github.com>
2017-08-18 14:46:49 +0800

    Update GitMain.java

--- src/main/java/cn/sh/hs/GitMain.java
+++ src/main/java/cn/sh/hs/GitMain.java
@@ -17,15 +17,6 @@ public class GitMain {
 
         System.out.print("创建了分支v2  333333333333333333333333");
 
-        System.out.print("在本地进行了分支，修改了代码");
-
-
-        System.out.print("最后更改的···········");
-
-        System.out.print("测试冲突11111+333333333333");
-
-        System.out.print("测试冲突11111+22222222222222222");
-
-        System.out.print("我先更新lllllllllllllll1113333");
+      
     }
 }
