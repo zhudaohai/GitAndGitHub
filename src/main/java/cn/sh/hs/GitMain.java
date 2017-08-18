3575c8b0e9d86f42f83e4edc11509e01284c3bd8
dhzhu <dhzhu@huashengwed.com>
2017-08-18 15:29:04 +0800

    aaaaaaaaaaa

--- src/main/java/cn/sh/hs/GitMain.java
+++ src/main/java/cn/sh/hs/GitMain.java
@@ -10,6 +10,7 @@ public class GitMain {
 
     public static void main(String[] args) {
         System.out.print("新创的一个开发项目！11111111111122");
+        System.out.print("a'a'a'a'a'a'a'a'a'a'a'a'a");
      
     }
 }
