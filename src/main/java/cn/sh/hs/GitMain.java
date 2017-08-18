1ac64c8d5a85176c11731f4b4ee56706a2f69fa4
dhzhu <dhzhu@huashengwed.com>
2017-08-18 10:44:39 +0800

    提交一个新的项目测试gitandgithub

---
+++ src/main/java/cn/sh/hs/GitMain.java
@@ -0,0 +1,14 @@
+package cn.sh.hs;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author Ricky Fung
+ * @create 2017-08-18 10:21
+ */
+public class GitMain {
+
+    public static void main(String[] args){
+        System.out.print("新创的一个开发项目！");
+    }
+}
