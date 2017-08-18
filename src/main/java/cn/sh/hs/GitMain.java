b0feb6af0ff7d1ff46dc5d238a586513b9ecd572
dhzhu <dhzhu@huashengwed.com>
2017-08-18 16:56:56 +0800

    99999

--- src/main/java/cn/sh/hs/GitMain.java
+++ src/main/java/cn/sh/hs/GitMain.java
@@ -9,7 +9,7 @@ package cn.sh.hs;
 public class GitMain {
 
     public static void main(String[] args) {
-        System.out.print("新创的一个开发项目!");
+        System.out.print("新创的一个开发项目！111111111111226666663335555");
 
 
 
